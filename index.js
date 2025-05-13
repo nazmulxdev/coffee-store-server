086cf17b503510bd90469e386c5cf2058fc15c62
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-05-13 22:14:05 +0600

    secured database user name and password

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import "dotenv/config";
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD} @cluster0.qtoljre.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Coffee server is getting ready");
